e782cdea9045aae3a55598be524904b77d1de649
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-08-31 20:00:46 +0200

    Phase29: register explicit curriculum UI query

--- src/Edulytics.Web/Extensions/CurriculumRegistrationExtensions.cs
+++ src/Edulytics.Web/Extensions/CurriculumRegistrationExtensions.cs
@@ -13,6 +13,7 @@ public static class CurriculumRegistrationExtensions
         services.AddScoped<ICurriculumRepository, CurriculumRepository>();
         services.AddScoped<ICurriculumService, CurriculumService>();
         services.AddScoped<IExplicitCurriculumLevelService, ExplicitCurriculumLevelService>();
+        services.AddScoped<IExplicitCurriculumLevelUiQuery, ExplicitCurriculumLevelUiQuery>();
         services.AddScoped<MathematicsCurriculumPackSeeder>();
         return services;
     }
