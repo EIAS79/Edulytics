13d72659c4284ee5d1fb72f8f6adb4789ed85bf0
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-08-31 19:31:37 +0200

    phase29: configure outcome curriculum adoption identity

--- src/Edulytics.Data/Configurations/LearningOutcomeConfiguration.cs
+++ src/Edulytics.Data/Configurations/LearningOutcomeConfiguration.cs
@@ -1,18 +1,82 @@
 using Edulytics.Core.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace Edulytics.Data.Configurations;
+
 public sealed class LearningOutcomeConfiguration : IEntityTypeConfiguration<LearningOutcome>
 {
- public void Configure(EntityTypeBuilder<LearningOutcome> b)
- {
-  b.ToTable("LearningOutcomes"); b.HasKey(x=>x.Id); b.HasAlternateKey(x=>new{x.SchoolId,x.Id});
-  b.Property(x=>x.Code).HasMaxLength(300).IsRequired(); b.Property(x=>x.Description).HasMaxLength(1000).IsRequired(); b.Property(x=>x.Weight).HasPrecision(6,3).IsRequired(); b.Property(x=>x.Order).IsRequired();
-  b.HasIndex(x=>new{x.SchoolId,x.AcademicProgramId,x.FrameworkVersionId,x.SubjectId,x.GradeLevelId,x.Code}).IsUnique();
-  b.HasIndex(x=>new{x.SchoolId,x.TopicId,x.OfficialContentNodeId}).IsUnique(); b.HasIndex(x=>new{x.SchoolId,x.TopicId,x.Order}).IsUnique();
-  b.HasOne<School>().WithMany().HasForeignKey(x=>x.SchoolId).OnDelete(DeleteBehavior.Restrict);
-  b.HasOne<AcademicProgram>().WithMany().HasForeignKey(x=>new{x.SchoolId,x.AcademicProgramId}).HasPrincipalKey(x=>new{x.SchoolId,x.Id}).OnDelete(DeleteBehavior.Restrict);
-  b.HasOne<CurriculumTopic>().WithMany().HasForeignKey(x=>new{x.SchoolId,x.AcademicProgramId,x.FrameworkVersionId,x.SubjectId,x.GradeLevelId,x.TopicId}).HasPrincipalKey(x=>new{x.SchoolId,x.AcademicProgramId,x.FrameworkVersionId,x.SubjectId,x.GradeLevelId,x.Id}).OnDelete(DeleteBehavior.Restrict);
-  b.HasOne<CurriculumPackContentNode>().WithMany().HasForeignKey(x=>x.OfficialContentNodeId).OnDelete(DeleteBehavior.Restrict);
- }
+    public void Configure(EntityTypeBuilder<LearningOutcome> b)
+    {
+        b.ToTable("LearningOutcomes");
+        b.HasKey(x => x.Id);
+        b.HasAlternateKey(x => new { x.SchoolId, x.Id });
+        b.Property(x => x.Code).HasMaxLength(300).IsRequired();
+        b.Property(x => x.Description).HasMaxLength(1000).IsRequired();
+        b.Property(x => x.Weight).HasPrecision(6, 3).IsRequired();
+        b.Property(x => x.Order).IsRequired();
+
+        b.HasIndex(x => new { x.SchoolId, x.CurriculumAdoptionId, x.Code })
+            .IsUnique()
+            .HasDatabaseName("UX_LearningOutcome_Adoption_Code")
+            .HasFilter("\"CurriculumAdoptionId\" IS NOT NULL");
+
+        b.HasIndex(x => new
+            {
+                x.SchoolId,
+                x.AcademicProgramId,
+                x.FrameworkVersionId,
+                x.SubjectId,
+                x.GradeLevelId,
+                x.Code
+            })
+            .IsUnique()
+            .HasDatabaseName("UX_LearningOutcome_Legacy_Code")
+            .HasFilter("\"CurriculumAdoptionId\" IS NULL");
+
+        b.HasIndex(x => new { x.SchoolId, x.TopicId, x.OfficialContentNodeId })
+            .IsUnique();
+        b.HasIndex(x => new { x.SchoolId, x.TopicId, x.Order })
+            .IsUnique();
+
+        b.HasOne<School>()
+            .WithMany()
+            .HasForeignKey(x => x.SchoolId)
+            .OnDelete(DeleteBehavior.Restrict);
+        b.HasOne<AcademicProgram>()
+            .WithMany()
+            .HasForeignKey(x => new { x.SchoolId, x.AcademicProgramId })
+            .HasPrincipalKey(x => new { x.SchoolId, x.Id })
+            .OnDelete(DeleteBehavior.Restrict);
+        b.HasOne<CurriculumTopic>()
+            .WithMany()
+            .HasForeignKey(x => new
+            {
+                x.SchoolId,
+                x.AcademicProgramId,
+                x.FrameworkVersionId,
+                x.SubjectId,
+                x.GradeLevelId,
+                x.TopicId
+            })
+            .HasPrincipalKey(x => new
+            {
+                x.SchoolId,
+                x.AcademicProgramId,
+                x.FrameworkVersionId,
+                x.SubjectId,
+                x.GradeLevelId,
+                x.Id
+            })
+            .OnDelete(DeleteBehavior.Restrict);
+        b.HasOne<SchoolCurriculumAdoption>()
+            .WithMany()
+            .HasForeignKey(x => new { x.SchoolId, x.CurriculumAdoptionId })
+            .HasPrincipalKey(x => new { x.SchoolId, x.Id })
+            .OnDelete(DeleteBehavior.Restrict);
+        b.HasOne<CurriculumPackContentNode>()
+            .WithMany()
+            .HasForeignKey(x => x.OfficialContentNodeId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
