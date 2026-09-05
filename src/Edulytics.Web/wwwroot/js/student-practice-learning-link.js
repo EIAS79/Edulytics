cde58ee17366c7aa1c9558608e687501e6393e48
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-09-05 23:12:34 +0200

    wip: accept all valid GUID text for mapped focus nodes

--- src/Edulytics.Web/wwwroot/js/student-practice-learning-link.js
+++ src/Edulytics.Web/wwwroot/js/student-practice-learning-link.js
@@ -9,7 +9,7 @@
     const baseHref = link.getAttribute('href');
     if (!baseHref) return;
 
-    const guidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    const guidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
     const focusIds = select => {
         const raw = select.selectedOptions[0]?.dataset.learningFocusNodeIds ?? '';
