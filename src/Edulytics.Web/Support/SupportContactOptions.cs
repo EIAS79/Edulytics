ca5a72246a7d9a0ef800c4a3d663c58e2bdc0014
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-09-05 21:05:54 +0200

    Acceptance: add centralized bilingual contact support surface

---
+++ src/Edulytics.Web/Support/SupportContactOptions.cs
@@ -0,0 +1,12 @@
+namespace Edulytics.Web.Support;
+
+public static class SupportContactOptions
+{
+    public const string Email = "info@our-cs.com";
+    public const string PhoneDisplay = "+48 514 132 428";
+    public const string PhoneUri = "tel:+48514132428";
+    public const string CompanyWebsiteDisplay = "our-cs.com";
+    public const string CompanyWebsiteUrl = "https://our-cs.com";
+    public const string ProductWebsiteDisplay = "edulytiks.com";
+    public const string ProductWebsiteUrl = "https://edulytiks.com";
+}
