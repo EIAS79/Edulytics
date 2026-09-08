e66903d757b737467b76f5a5d09fff90da37c79d
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-09-09 01:44:26 +0200

    feat: add public sales demo and help routes

--- src/Edulytics.Web/Controllers/ContactController.cs
+++ src/Edulytics.Web/Controllers/ContactController.cs
@@ -3,10 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Edulytics.Web.Controllers;
 
+[AllowAnonymous]
 [Route("contact")]
 public sealed class ContactController : Controller
 {
-    [AllowAnonymous]
     [HttpGet("")]
     public IActionResult Index() => View();
+
+    [HttpGet("sales-enquiry")]
+    public IActionResult SalesEnquiry() => View("Inquiry", "sales");
+
+    [HttpGet("request-demo")]
+    public IActionResult RequestDemo() => View("Inquiry", "demo");
+
+    [HttpGet("help")]
+    public IActionResult HelpAlias() => Redirect("/help");
+
+    [HttpGet("/help")]
+    public IActionResult Help() => View();
 }
