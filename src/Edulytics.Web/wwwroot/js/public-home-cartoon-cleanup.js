860be8d6f8b80dbb874272ed8a263f38793818c6
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-09-12 09:22:45 +0200

    Use approved transparent mascot in home hero

--- src/Edulytics.Web/wwwroot/js/public-home-cartoon-cleanup.js
+++ src/Edulytics.Web/wwwroot/js/public-home-cartoon-cleanup.js
@@ -12,12 +12,11 @@
 
     if (!firstSlideVisual.querySelector('.ed-home-v16-mascot-canvas')) {
       const image = document.createElement('img');
-      image.className = 'ed-home-v16-mascot-canvas';
-      image.src = '/images/public/edulytics-math-mascot.png';
+      image.className = 'ed-home-v16-mascot-canvas ed-home-v17-mascot-image';
+      image.src = '/images/brand/edulytics-mascot-final.png?v=33';
       image.alt = 'Edulytics mathematics mascot';
       image.loading = 'eager';
       image.decoding = 'async';
-      image.style.width = '430px';
       firstSlideVisual.appendChild(image);
     }
   }
