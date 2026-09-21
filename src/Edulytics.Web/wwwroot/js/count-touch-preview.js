c309718739739cb12edcf0d5468af54007acbd24
Elioo <147754375+EIAS79@users.noreply.github.com>
2026-09-21 14:53:01 +0200

    Restore Eddy character identity in Practice

--- src/Edulytics.Web/wwwroot/js/count-touch-preview.js
+++ src/Edulytics.Web/wwwroot/js/count-touch-preview.js
@@ -166,7 +166,7 @@
             '</main>' +
             '<div class="cg-eddy-panel" data-cg-eddy-panel>' +
                 '<img src="' + config.guide + '" alt="Eddy, the Edulytics guide" />' +
-                '<div><span>EDULYTICS</span><p data-cg-eddy-text>' + copy.intro + '</p></div>' +
+                '<div><span>EDDY</span><p data-cg-eddy-text>' + copy.intro + '</p></div>' +
                 '<button class="cg-primary" type="button" data-cg-start>' + copy.start + '</button>' +
             '</div>' +
             '<div class="cg-complete" data-cg-complete hidden>' +
